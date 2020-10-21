5a9c71953a8bcd7b55426336cfcb862204c94976
sonpham-99 <73100814+sonpham-99@users.noreply.github.com>
2020-10-21 10:15:06 +0700

    b

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-server.listen(3000);
-
-app.get("/", function(req, res){
-    res.send("<font color = pink> Xin chào</font>")
-});
