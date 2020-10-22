a9ddeab34f5c8d9c6b5794df8135e30a262e8014
sonpham-99 <73100814+sonpham-99@users.noreply.github.com>
2020-10-22 12:46:21 +0700

    c

--- server.js
+++ server.js
@@ -7,5 +7,14 @@ var server = require("http").createServer(app);
 server.listen(1234);
 
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/.html'));
+    res.sendFile(path.join(__dirname+'/homepage.html'));
   });
+
+app.get('/homepage.html', function(req,res){
+    res.sendFile(path.join(__dirname+'/homepage.html'));
+  });
+
+
+app.use(express.static('public'));
+//app.use(express.static(__dirname + 'public'));
+console.debug('Server listening on port  ' + server.address().port)
